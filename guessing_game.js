bc9b057b69462f21aa4a2d91ebd2b6ecae338365
MathieuHoude <mathieu.houde@codeboxx.biz>
2022-02-08 10:29:33 -0500

    first

---
+++ guessing_game.js
@@ -0,0 +1,9 @@
+const main = () => {
+    const difficulties = [1, 2, 3, 4, 5];
+    let continue_playing = true;
+    console.log("Welcome to this fabulous number guessing game.");
+    while (continue_playing) {
+        console.log('Please choose a difficulty level (1 = VeryEasy / 2 = Easy / 3 = Medium / 4 = Hard / 5 = VeryHard)');
+
+    }
+}
